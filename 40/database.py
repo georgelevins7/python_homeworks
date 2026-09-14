10efcedef373a4d1d47eee41ece85ce210062ac1
georgelevins7 <georgelevins7@gmail.com>
2026-09-14 20:25:09 +0400

    Update database.py

--- 40/database.py
+++ 40/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-engine = create_engine("postgresql+psycopg2://postgres:REVE4TSM@localhost:5432/movie", echo=True)
+engine = create_engine("postgresql+psycopg2://postgres:1234@localhost:5432/movie", echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
